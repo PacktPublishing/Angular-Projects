7a260c919d27ac9452f14e30bcb7f0c0aefb76ac
Zama Khan Mohammed <zama.mohammed@tekzenit.com>
2019-02-26 15:03:33 -0600

    chapter 6 project

---
+++ Chapter 6/apartment-auditing/src/app/home/home.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+
+import { HomePage } from './home.page';
+import { RoomsAndBathsPipe } from '../rooms-and-baths.pipe';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HomePage
+      }
+    ])
+  ],
+  declarations: [HomePage, RoomsAndBathsPipe]
+})
+export class HomePageModule {}
