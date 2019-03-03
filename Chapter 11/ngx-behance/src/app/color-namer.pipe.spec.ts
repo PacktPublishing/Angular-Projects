b0f9c411feab8874ead0474c5f595dc0ab8fa51d
Zama Khan Mohammed <zama.mohammed@tekzenit.com>
2019-03-02 18:17:41 -0600

    test pipe

--- Chapter 11/ngx-behance/src/app/color-namer.pipe.spec.ts
+++ Chapter 11/ngx-behance/src/app/color-namer.pipe.spec.ts
@@ -5,4 +5,27 @@ describe('ColorNamerPipe', () => {
     const pipe = new ColorNamerPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should return current color', () => {
+    const pipe = new ColorNamerPipe();
+    let transformedValue = pipe.transform({
+      r: 255,
+      g: 255,
+      b: 255
+    });
+    expect(transformedValue).toEqual('white');
+    transformedValue = pipe.transform({
+      r: 255,
+      g: 0,
+      b: 0,
+    });
+    expect(transformedValue).toEqual('red');
+    transformedValue = pipe.transform({
+      r: 0,
+      g: 255,
+      b: 0,
+    });
+    expect(transformedValue).toEqual('lime');
+
+  });
 });
