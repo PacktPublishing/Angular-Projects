7cd9aa351487e89005f9b7bd066d8cb9fb740fa1
Zama Khan Mohammed <zama.mohammed@tekzenit.com>
2019-03-03 12:41:41 -0600

    add code coverage

--- Chapter 11/ngx-behance/src/karma.conf.js
+++ Chapter 11/ngx-behance/src/karma.conf.js
@@ -18,14 +18,22 @@ module.exports = function (config) {
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../coverage/ngx-behance'),
       reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+      fixWebpackSourcePaths: true,
+      thresholds: {
+        global: {
+          statements: 80,
+          branches: 80,
+          functions: 75,
+          lines: 80
+        }
+      }
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: false,
     restartOnFileChange: true
   });
