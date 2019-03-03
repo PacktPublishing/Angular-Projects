41f0e0307754eb71746e2270e21523843e221c3e
Zama Khan Mohammed <zama.mohammed@tekzenit.com>
2019-03-03 12:46:35 -0600

    add husky

--- Chapter 11/ngx-behance/src/app/post/post.component.ts
+++ Chapter 11/ngx-behance/src/app/post/post.component.ts
@@ -8,7 +8,7 @@ import { map } from 'rxjs/operators';
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
-export class PostComponent implements OnInit {
+export class PostComponent {
   post$;
   isFavorite: boolean;
   constructor(private behance: BehanceService, private route: ActivatedRoute) {
