429a0d1c49aeeaf901227e14d4d8e31fa63e4155
Zama Khan Mohammed <mohammedzamakhan@gmail.com>
2019-04-17 09:22:26 -0500

    update chapter 5 getProducts method

--- Chapter 5/es-api/src/products/products.controller.ts
+++ Chapter 5/es-api/src/products/products.controller.ts
@@ -1,12 +1,16 @@
 import { Get, Controller } from '@nestjs/common';
 import { ProductsService } from './products.service';
 
+const _ = require('lodash');
+
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Get()
   getProducts(): any {
-    return this.productsService.findAll();
+    return this.productsService.findAll().then(products => {
+      return products.map(product => _.omit(product.toJSON(), ['sales']));
+    });
   }
 }
