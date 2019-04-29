edbac700dce4db1ca00aff99ce3b5fcfcfd8a35d
Zama Khan Mohammed <mohammedzamakhan@gmail.com>
2019-04-29 13:26:37 -0500

    update chapter 3 to v8

--- Chapter 3/personal-blog/src/app/app-routing.module.ts
+++ Chapter 3/personal-blog/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent, data: {routeState: 1} },
   { path: 'about', component: AboutComponent, data: {routeState: 2} },
-  { path: 'post/:id/:slug', loadChildren: './post/post.module#PostModule', data: {routeState: 3} },
+  { path: 'post/:id/:slug', loadChildren: () => import('./post/post.module').then(m => m.PostModule), data: {routeState: 3} },
   { path: '**', component: HomeComponent }
 ];
 
